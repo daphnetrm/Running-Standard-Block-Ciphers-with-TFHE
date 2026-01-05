c796da06307989c4992c21b128642fd38679f96e
daphnetrm <78088487+daphnetrm@users.noreply.github.com>
2026-01-05 15:14:50 +0100

    Delete simon/build directory

--- simon/build/sources/CMakeFiles/simon.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for simon.
-# This may be replaced when dependencies are built.
--- simon/build/sources/CMakeFiles/simon.dir/key_schedule.cpp.o.d
+++
@@ -1,4 +0,0 @@
-sources/CMakeFiles/simon.dir/key_schedule.cpp.o: \
- /home/admin-lcyl/Documents/Thèse_A2/code_SIMON/simon_fhe/sources/key_schedule.cpp \
- /usr/include/stdc-predef.h \
- /home/admin-lcyl/Documents/Thèse_A2/code_SIMON/simon_fhe/sources/key_schedule.h
